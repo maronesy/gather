e286ce2ac8f092d626a42dd54e37c967d5f85227
maronesy <souhayl@gmail.com>
2016-02-03 23:35:19 -0800

    added simple junit and jasmin tests

---
+++ src/test/java/com/programcreek/helloworld/controller/HelloWorldControllerTest.java
@@ -0,0 +1,33 @@
+package com.programcreek.helloworld.controller;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+import java.util.List;
+import org.junit.*;
+
+
+public class HelloWorldControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testShowMessage1() {
+    	assertTrue(true);
+    }
+    
+    @Test
+    public void testShowMessage2() {
+    	assertTrue(true);
+    }
+    
+    @Test
+    public void testShowMessage3() {
+    	assertTrue(true);
+    }
+}
