24808b4f3314f87b5a6b4d01a391c72511284862
Benson Ma <bjma2@illinois.edu>
2016-02-14 15:43:12 +0900

    Merged in Souhayl's code from GitHub

--- src/main/java/cs428/project/gather/GatherApplication.java
+++ src/main/java/cs428/project/gather/GatherApplication.java
@@ -3,16 +3,22 @@ package cs428.project.gather;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @EnableAutoConfiguration(exclude = {
-org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
-org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration.class
+    org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
+    org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration.class
 })
 
 @SpringBootApplication
-public class GatherApplication {
+public class GatherApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GatherApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatherApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatherApplication.class, args);
+    }
 }
