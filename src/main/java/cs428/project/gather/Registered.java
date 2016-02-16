78ce53226f0aff3d76e53cfd6db6157d30d28e53
Benson Ma <bjma2@illinois.edu>
2016-02-16 13:20:19 +0900

    added new attribute

--- src/main/java/cs428/project/gather/Registered.java
+++ src/main/java/cs428/project/gather/Registered.java
@@ -10,12 +10,14 @@ public class Registered {
 	private String firstName;
 	private String lastName;
 	private int age;
+	private String location;
 
 	private Registered() {}
 
-	public Registered(String firstName, String lastName, int age) {
+	public Registered(String firstName, String lastName, int age, String location) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.age = age;
+		this.location = location;
 	}
 }
