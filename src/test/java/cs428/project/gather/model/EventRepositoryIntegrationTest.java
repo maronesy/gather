f0e1657b1e54192e6f74e61a31fe7d2c0e07cf9f
Kai Song <ksong@lbl.gov>
2016-02-25 01:48:20 -0600

    implemented first JUnit test for JPA

---
+++ src/test/java/cs428/project/gather/model/EventRepositoryIntegrationTest.java
@@ -0,0 +1,38 @@
+package cs428.project.gather.model;
+
+//import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import cs428.project.gather.GatherApplication;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(GatherApplication.class)
+@WebAppConfiguration
+@ActiveProfiles("scratch")
+public class EventRepositoryIntegrationTest {
+	@Autowired
+	EventRepository repository;
+
+	@Test
+	public void savesCustomerCorrectly() {
+
+		Event testEvent = new Event("Test Event");
+		Occurrence occur=new Occurrence("Single Occurrence",new Timestamp(Calendar.getInstance().getTime().getTime()));
+		testEvent.add(occur);
+		Event result = this.repository.save(testEvent);
+		
+		assertTrue(result.getDescription().equals("Test Event"));
+	}
+}
