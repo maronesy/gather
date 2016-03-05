3e61560346d90f2e67128cc9298de978ffdb18c5
Kai Song <ksong@lbl.gov>
2016-03-05 01:04:48 -0600

    mgabreski2_kaisong2 Implemented validators for user registration.

---
+++ src/main/java/cs428/project/gather/data/RESTResponseData.java
@@ -0,0 +1,112 @@
+package cs428.project.gather.data;
+
+
+import java.util.Date;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+
+public class RESTResponseData {
+
+	private int status = -1;
+	private String message = "";
+	private Date timestamp;
+	
+	public RESTResponseData(int status){
+		this.status = status;
+		this.timestamp = new Date();
+	}
+	
+	public RESTResponseData(int status, String message){
+		this.status = status;
+		this.timestamp = new Date();
+		this.message = message;
+	}
+	
+	public RESTResponseData(BindingResult error){
+		String message="";
+		int errorCode=-1;
+		int count=0;
+		for (Object object : error.getAllErrors()) {
+		    if(object instanceof FieldError) {
+		        FieldError fieldError = (FieldError) object;
+		        message+=fieldError.getDefaultMessage()+" ";
+		        errorCode=Integer.parseInt(fieldError.getCode());
+		    }
+
+		    if(object instanceof ObjectError) {
+		        ObjectError objectError = (ObjectError) object;
+		        message+=objectError.getDefaultMessage()+" ";
+		        errorCode=Integer.parseInt(objectError.getCode());
+		    }
+		    count++;
+		}
+		if(count>1){
+			this.status=-100;
+		}else{
+			this.status = errorCode;
+		}
+		this.timestamp = new Date();
+		this.message = message;
+	}
+
+	public int getSTATUS() {
+		return status;
+	}
+
+	public void setSTATUS(int status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		HashCodeBuilder builder = new HashCodeBuilder();
+
+		builder.append(this.status);
+		builder.append(this.message);
+
+		int hashCode = builder.toHashCode();
+
+		return hashCode;
+	}
+
+	@Override
+	public boolean equals(Object anotherObject) {
+		boolean equal = false;
+
+		if (anotherObject == this) {
+			equal = true;
+		} else if (anotherObject != null && anotherObject.getClass().equals(this.getClass())) {
+			RESTResponseData anotherSignInData = (RESTResponseData) anotherObject;
+
+			EqualsBuilder equalsBuilder = new EqualsBuilder();
+
+			equalsBuilder.append(this.status, anotherSignInData.status);
+			equalsBuilder.append(this.message, anotherSignInData.message);
+			
+
+			equal = equalsBuilder.isEquals();
+		}
+		return equal;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+}
