9aab54b73494a7baf930803d61ccf26138235f06
Kai Song <ksong@lbl.gov>
2016-03-06 22:48:20 -0600

    added test and use HTTP header signout controller

--- src/main/java/cs428/project/gather/controllers/SignOutController.java
+++ src/main/java/cs428/project/gather/controllers/SignOutController.java
@@ -3,6 +3,8 @@ package cs428.project.gather.controllers;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +15,14 @@ import cs428.project.gather.utilities.SignOutHelper;
 @Controller("signOutController")
 public class SignOutController
 {
-	@RequestMapping(value="/sign-out")
-	public String signOut(HttpServletRequest request, HttpServletResponse response)
+	@RequestMapping(value="/api/sign-out")
+	public ResponseEntity<String> signOut(HttpServletRequest request, HttpServletResponse response)
 	{
-		SignOutHelper.invalidateSession(request);
+		if(SignOutHelper.invalidateSession(request)
+				&& SignOutHelper.deleteSessionCookie(request, response)){
+			return new ResponseEntity<String>(HttpStatus.OK); 
+		};
 
-		SignOutHelper.deleteSessionCookie(request, response);
-
-		return "sign-out-complete";
+		return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 	}
 }
---
+++ src/test/java/cs428/project/gather/controller/rest/SignOutControllerTest.java
@@ -0,0 +1,75 @@
+package cs428.project.gather.controller.rest;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import cs428.project.gather.GatherApplication;
+import cs428.project.gather.data.model.Registrant;
+import cs428.project.gather.data.repo.EventRepository;
+import cs428.project.gather.data.repo.RegistrantRepository;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(GatherApplication.class)
+@WebIntegrationTest
+public class SignOutControllerTest {
+
+	public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+	private RestTemplate restTemplate = new TestRestTemplate();
+
+	@Autowired
+	RegistrantRepository registrantRepo;
+
+	@Autowired
+	EventRepository eventRepo;
+
+	@Before
+	public void setUp() {
+		eventRepo.deleteAll();
+		assertEquals(this.eventRepo.count(), 0);
+		registrantRepo.deleteAll();
+		assertEquals(this.registrantRepo.count(), 0);
+		Registrant aUser = new Registrant("existed@email.com", "password", "existedName", 10L, 3, 10000);
+		this.registrantRepo.save(aUser);
+		assertEquals(this.registrantRepo.count(), 1);
+	}
+	
+	//TODO Mock a session for logged in user to test the case of success
+	
+	@Test
+	public void testSignOutUserFail() throws JsonProcessingException {
+		ResponseEntity<String> response = signOutUser("existed@email.com", "password");
+		assertTrue(response.getStatusCode().equals(HttpStatus.BAD_REQUEST));
+
+		//TODO Need to further confirm the session is updated correctly
+	}
+	
+	private ResponseEntity<String> signOutUser(String email, String password) throws JsonProcessingException {
+		
+		// Invoking the API
+		ResponseEntity<String> returnHeader = restTemplate.exchange("http://localhost:8888/api/sign-out", HttpMethod.GET, null, String.class);
+
+		assertNotNull(returnHeader);
+		
+		// Asserting the response of the API.
+		return returnHeader;
+
+	}
+
+}
