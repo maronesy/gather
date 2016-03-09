c817959fe92834ee8ae0f5360a56a4074ffd3784
Kai Song <ksong@lbl.gov>
2016-03-09 01:36:54 -0600

    Added session response data been for including displayName

--- src/main/java/cs428/project/gather/controllers/SessionController.java
+++ src/main/java/cs428/project/gather/controllers/SessionController.java
@@ -3,26 +3,34 @@ package cs428.project.gather.controllers;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import cs428.project.gather.data.RESTResponseData;
+import cs428.project.gather.data.RESTSessionResponseData;
+import cs428.project.gather.data.model.Actor;
+import cs428.project.gather.data.repo.RegistrantRepository;
 import cs428.project.gather.utilities.ActorStateUtility;
 
 
 @Controller("sessionController")
 public class SessionController
 {
+	@Autowired
+	private RegistrantRepository registrantDataAdapter;
+	
 	@RequestMapping(value="/api/session")
-	public ResponseEntity<RESTResponseData> getSession(HttpServletRequest request, HttpServletResponse response)
+	public ResponseEntity<RESTSessionResponseData> getSession(HttpServletRequest request, HttpServletResponse response)
 	{
 		if(ActorStateUtility.retrieveAuthenticatedStateInRequest(request)){
-			return new ResponseEntity<RESTResponseData>(new RESTResponseData(5,"Session Found"),HttpStatus.FOUND);
+			Actor actor = ActorStateUtility.retrieveActorFromRequest(request);
+			String displayName = this.registrantDataAdapter.findOne(actor.getActorID()).getDisplayName();
+			return new ResponseEntity<RESTSessionResponseData>(new RESTSessionResponseData(5,"Session Found",displayName),HttpStatus.FOUND);
 		}else{
-			return new ResponseEntity<RESTResponseData>(new RESTResponseData(-5,"Session Not Found"),HttpStatus.NOT_FOUND);
+			return new ResponseEntity<RESTSessionResponseData>(new RESTSessionResponseData(-5,"Session Not Found"),HttpStatus.NOT_FOUND);
 		}
 	}
 }
