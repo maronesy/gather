ed1a21e33a64cd3cf740144d4a9d13e783879967
Heeho Park <hdpark2@illinois.edu>
2016-03-15 01:09:26 -0600

    hdpark2_marones2 listing events from the backend

--- src/main/resources/static/scripts/map.js
+++ src/main/resources/static/scripts/map.js
@@ -132,7 +132,6 @@ function MapManager(mapboxAccessToken, mapboxMapID) {
 
 				placeUserMarker(userCoordinates);
 				getNearByEvents(userCoordinates);
-
 				currentUserCoordinates = userCoordinates;
 			}
 		}
@@ -581,7 +580,7 @@ function MapManager(mapboxAccessToken, mapboxMapID) {
 							}
 					placeEstablishedEventMarker(gather.global.nearEvents[i], true);
 				}
-				
+				loadEventsFirstView(userCoordinates);
 			},
 			error: function(jqXHR, textStatus, errorThrown) {
 //			    alert(jqXHR.status);
@@ -600,21 +599,6 @@ function MapManager(mapboxAccessToken, mapboxMapID) {
 			}
 		});
 	}
-	
-	function distance(lat1, lon1, lat2, lon2, unit) {
-		var radlat1 = Math.PI * lat1/180
-		var radlat2 = Math.PI * lat2/180
-		var theta = lon1-lon2
-		var radtheta = Math.PI * theta/180
-		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-		dist = Math.acos(dist)
-		dist = dist * 180/Math.PI
-		dist = dist * 60 * 1.1515
-		if (unit=="K") { dist = dist * 1.609344 }
-		if (unit=="N") { dist = dist * 0.8684 }
-		return dist
-	}
-
 	
 	this.determineCoordByZipCode = function(zipCode) {
 		
@@ -701,4 +685,18 @@ function determineCoordByZipCode1(zipCode) {
     } else {
     	return 0;
     }
+}
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+	var radlat1 = Math.PI * lat1/180
+	var radlat2 = Math.PI * lat2/180
+	var theta = lon1-lon2
+	var radtheta = Math.PI * theta/180
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist)
+	dist = dist * 180/Math.PI
+	dist = dist * 60 * 1.1515
+	if (unit=="K") { dist = dist * 1.609344 }
+	if (unit=="N") { dist = dist * 0.8684 }
+	return dist
 }
