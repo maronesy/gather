20790663f5e258bf0244a21740ca2bbb9be11aa4
maronesy <souhayl@gmail.com>
2016-04-24 20:59:12 -0700

    added selenium test for Signout and Registration

--- src/test/java/cs428/project/gather/selenium/UserLogoutTest.java
+++ src/test/java/cs428/project/gather/selenium/UserLogoutTest.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -20,7 +19,7 @@ import cs428.project.gather.GatherApplication;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(GatherApplication.class)
 @WebIntegrationTest
-public class UserLoginTest {
+public class UserLogoutTest {
 	private static FirefoxDriver driver;
  	WebElement element;
 
@@ -32,40 +31,26 @@ public class UserLoginTest {
 	} 
 
  @Test
- public void valid_UserCredential(){
-
-	 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-     driver.get("http://localhost:8888");
-     driver.findElement(By.id("signInEmail")).sendKeys("testuser@email.com");
-     driver.findElement(By.id("signInPassword")).sendKeys("password");
-     driver.findElement(By.id("loginFormSubmit")).click();
-     try{
-    	 String text="Welcome testDisplayName";
-    	 element = driver.findElement (By.xpath("//*[contains(text(),'" + text + "')]"));
-	 }catch (Exception e){
-		}
-     Assert.assertNotNull(element);
-     System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
- }
-
- @Test
- public void inValid_UserEmail()
+ public void logoutTest()
  {
 	 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
      driver.get("http://localhost:8888");	
-     driver.findElement(By.id("signInEmail")).sendKeys("testuserBadEmail");
+     driver.findElement(By.id("signInEmail")).sendKeys("testuser@email.com");
      driver.findElement(By.id("signInPassword")).sendKeys("password");
      driver.findElement(By.id("loginFormSubmit")).click();
+     driver.findElement(By.id("signOutButton")).click();
+//     String outText = "Sign Out";
+//	 driver.findElement (By.xpath("//*[contains(text(),'" + outText + "')]")).click();
      try{
 		 //element = driver.findElement (By.xpath(".//*[@id='loginErrorBox']/a"));
-    	 String text="Please enter a valid email address";
+    	 String text="Sign In";
     	 element = driver.findElement (By.xpath("//*[contains(text(),'" + text + "')]"));
 	 }catch (Exception e){
 		}
      Assert.assertNotNull(element);
      System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
  }
- 
+
  @AfterClass
  public static void closeBrowser(){
 	 driver.quit();
--- src/test/java/cs428/project/gather/selenium/UserRegistrationTest.java
+++ src/test/java/cs428/project/gather/selenium/UserRegistrationTest.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -20,7 +19,7 @@ import cs428.project.gather.GatherApplication;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(GatherApplication.class)
 @WebIntegrationTest
-public class UserLoginTest {
+public class UserRegistrationTest {
 	private static FirefoxDriver driver;
  	WebElement element;
 
@@ -30,42 +29,30 @@ public class UserLoginTest {
      driver = new FirefoxDriver();
      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	} 
-
+ 
  @Test
- public void valid_UserCredential(){
-
-	 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-     driver.get("http://localhost:8888");
-     driver.findElement(By.id("signInEmail")).sendKeys("testuser@email.com");
-     driver.findElement(By.id("signInPassword")).sendKeys("password");
-     driver.findElement(By.id("loginFormSubmit")).click();
-     try{
-    	 String text="Welcome testDisplayName";
-    	 element = driver.findElement (By.xpath("//*[contains(text(),'" + text + "')]"));
-	 }catch (Exception e){
-		}
-     Assert.assertNotNull(element);
-     System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
- }
-
- @Test
- public void inValid_UserEmail()
+ public void registrationTest()
  {
 	 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-     driver.get("http://localhost:8888");	
-     driver.findElement(By.id("signInEmail")).sendKeys("testuserBadEmail");
-     driver.findElement(By.id("signInPassword")).sendKeys("password");
-     driver.findElement(By.id("loginFormSubmit")).click();
+     driver.get("http://localhost:8888");
+     String regText = "Register";
+	 driver.findElement (By.xpath("//*[contains(text(),'" + regText + "')]")).click();
+     driver.findElement(By.id("inputEmail")).sendKeys("jackiechan@gmail.com");
+     driver.findElement(By.id("inputPassword1")).sendKeys("password");
+     driver.findElement(By.id("inputPassword2")).sendKeys("password");
+     driver.findElement(By.id("inputDisplayName")).sendKeys("jackie");
+     driver.findElement(By.id("registerFormSubmit")).click();
+//     String subReg = "Submit";
+//     driver.findElement (By.xpath("//*[contains(text(),'" + subReg + "')]")).click();
      try{
-		 //element = driver.findElement (By.xpath(".//*[@id='loginErrorBox']/a"));
-    	 String text="Please enter a valid email address";
+    	 String text="Welcome jackie";
     	 element = driver.findElement (By.xpath("//*[contains(text(),'" + text + "')]"));
 	 }catch (Exception e){
 		}
      Assert.assertNotNull(element);
      System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
  }
- 
+
  @AfterClass
  public static void closeBrowser(){
 	 driver.quit();
