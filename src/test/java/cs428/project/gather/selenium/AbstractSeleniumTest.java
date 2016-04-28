a3322a46057c7545ee7df3d8b1b71adb2dd103da
Kai Song <ksong@lbl.gov>
2016-04-27 22:49:17 -0500

    refactored by adding abstract class for selemenium tests. copy functions for signIn and signOut added

---
+++ src/test/java/cs428/project/gather/selenium/AbstractSeleniumTest.java
@@ -0,0 +1,44 @@
+package cs428.project.gather.selenium;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public abstract class AbstractSeleniumTest {
+
+	protected static FirefoxDriver driver;
+	protected WebElement element;
+ 	
+ 	protected void userSignOut() {
+		driver.findElement(By.id("signOutButton")).click();
+	    boolean elemHidden = isElementHidden(driver.findElement(By.id("loginFormSubmit")));
+	    Assert.assertFalse(elemHidden);
+	}
+
+ 	protected void userSignIn() {
+		
+		driver.get("http://localhost:8888");
+	    
+	    driver.findElement(By.id("signInEmail")).sendKeys("testuser@email.com");
+	    driver.findElement(By.id("signInPassword")).sendKeys("password");
+	    driver.findElement(By.id("loginFormSubmit")).click();
+
+	    try{
+	    	String text="Welcome testDisplayName";
+	    	element = driver.findElement (By.xpath("//*[contains(text(),'" + text + "')]"));
+		}catch (Exception e){
+		}
+	    Assert.assertNotNull(element);
+	}
+	
+ 	protected boolean isElementHidden(WebElement welem){
+		 boolean clickFailed=false;
+	     try{
+	    	 welem.click();
+	     }catch(Exception e){
+	    	 clickFailed = true; 
+	     }
+	     return clickFailed;
+	 }
+}
--- src/test/java/cs428/project/gather/selenium/UserLoginTest.java
+++ src/test/java/cs428/project/gather/selenium/UserLoginTest.java
@@ -1,6 +1,5 @@
 package cs428.project.gather.selenium;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
@@ -9,11 +8,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -23,9 +18,7 @@ import cs428.project.gather.GatherApplication;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(GatherApplication.class)
 @WebIntegrationTest
-public class UserLoginTest {
-	private static FirefoxDriver driver;
- 	WebElement element;
+public class UserLoginTest extends AbstractSeleniumTest{
 
  @BeforeClass
  public static void openBrowser(){
@@ -38,32 +31,9 @@ public class UserLoginTest {
  public void valid_UserCredential(){
 
 	 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-	 driver.get("http://localhost:8888");
-     
-     driver.findElement(By.id("signInEmail")).sendKeys("testuser@email.com");
-     driver.findElement(By.id("signInPassword")).sendKeys("password");
-     driver.findElement(By.id("loginFormSubmit")).click();
-
-     try{
-    	 String text="Welcome testDisplayName";
-    	 element = driver.findElement (By.xpath("//*[contains(text(),'" + text + "')]"));
-	 }catch (Exception e){
-		}
-     Assert.assertNotNull(element);
-     driver.findElement(By.id("signOutButton")).click();
-     boolean elemHidden = isElementHidden(driver.findElement(By.id("loginFormSubmit")));
-     Assert.assertFalse(elemHidden);
+	 userSignIn();
+     userSignOut();
      System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
- }
-
- private boolean isElementHidden(WebElement welem){
-	 boolean clickFailed=false;
-     try{
-    	 welem.click();
-     }catch(Exception e){
-    	 clickFailed = true; 
-     }
-     return clickFailed;
  }
  
  @Test
@@ -75,7 +45,6 @@ public class UserLoginTest {
      driver.findElement(By.id("signInPassword")).sendKeys("password");
      driver.findElement(By.id("loginFormSubmit")).click();
      try{
-		 //element = driver.findElement (By.xpath(".//*[@id='loginErrorBox']/a"));
     	 String text="Please enter a valid email address";
     	 element = driver.findElement (By.xpath("//*[contains(text(),'" + text + "')]"));
 	 }catch (Exception e){
