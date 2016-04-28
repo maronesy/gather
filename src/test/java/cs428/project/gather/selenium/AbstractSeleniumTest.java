307e95586e4211ac3d10baf3e09bdcc96e8ede03
Kai Song <ksong@lbl.gov>
2016-04-28 10:40:57 -0500

    further refactored create event into a method in abstract class

--- src/test/java/cs428/project/gather/selenium/AbstractSeleniumTest.java
+++ src/test/java/cs428/project/gather/selenium/AbstractSeleniumTest.java
@@ -1,10 +1,15 @@
 package cs428.project.gather.selenium;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class AbstractSeleniumTest {
@@ -34,6 +39,33 @@ public abstract class AbstractSeleniumTest {
 	    Assert.assertNotNull(element);
 	}
 	
+
+ 	 protected void createNewEventWith(String name, String description, String category, int zipCode) {
+ 		driver.findElement(By.id("zipCode")).sendKeys(Integer.toString(zipCode));
+ 		 driver.findElement(By.id("enterZip")).click();
+ 		 this.timeoutFor(1500);
+ 		 driver.findElement(By.id("addEventBtn")).click();
+ 		 WebDriverWait wait = new WebDriverWait(driver, 3);
+ 		 WebElement updateEventInfoBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("updateEventInfoBtn")));
+ 		 
+ 		 updateEventInfoBtn.click();
+ 		 
+ 		 driver.findElement(By.id("event-name")).sendKeys(name);
+ 		 driver.findElement(By.id("event-description")).sendKeys(description);
+ 		 
+ 		 Select select = new Select(driver.findElement(By.id("event-category")));
+ 		 select.selectByValue(category);
+ 		 
+ 		 DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+ 		 Date today = new Date();
+ 		 Date tomorrow = new Date(today.getTime() + (1000 * 60 * 60 * 24));
+ 		 driver.findElement(By.id("event-occurrence1")).sendKeys(dateFormat.format(tomorrow));
+ 		 this.timeoutFor(1000);
+ 		 driver.findElement(By.id("event-save")).click();
+ 		 
+ 		 Assert.assertTrue(driver.getPageSource().contains(description));
+ 	 }
+ 	
  	protected boolean isElementHidden(WebElement welem){
 		 boolean clickFailed=false;
 	     try{
--- src/test/java/cs428/project/gather/selenium/EventBasicTest.java
+++ src/test/java/cs428/project/gather/selenium/EventBasicTest.java
@@ -1,8 +1,5 @@
 package cs428.project.gather.selenium;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
@@ -11,11 +8,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -39,37 +32,7 @@ public class EventBasicTest extends AbstractSeleniumTest {
 	 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
 	 driver.get("http://localhost:8888");
 	 userSignIn();
-	 driver.findElement(By.id("zipCode")).sendKeys("94704");
-	 driver.findElement(By.id("enterZip")).click();
-	 try {
-		 Thread.sleep(1500);
-	 } catch (InterruptedException e) {
-		 e.printStackTrace();
-	 }
-	 driver.findElement(By.id("addEventBtn")).click();
-	 WebDriverWait wait = new WebDriverWait(driver, 3);
-	 WebElement updateEventInfoBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("updateEventInfoBtn")));
-	 
-	 updateEventInfoBtn.click();
-	 
-	 driver.findElement(By.id("event-name")).sendKeys("New Event");
-	 driver.findElement(By.id("event-description")).sendKeys("this is a new event");
-	 
-	 Select select = new Select(driver.findElement(By.id("event-category")));
-	 select.selectByValue("Soccer");
-	 
-	 DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-	 Date today = new Date();
-	 Date tomorrow = new Date(today.getTime() + (1000 * 60 * 60 * 24));
-	 driver.findElement(By.id("event-occurrence1")).sendKeys(dateFormat.format(tomorrow));
-	 try {
-		 Thread.sleep(1000);
-	 } catch (InterruptedException e) {
-		 e.printStackTrace();
-	 }
-	 driver.findElement(By.id("event-save")).click();
-	 
-	 Assert.assertTrue(driver.getPageSource().contains("this is a new event"));
+	 createNewEventWith("New Event", "this is a new event", "Soccer", 94704);
 	 
 	 userSignOut();
 	 System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
