57b49111057bda9e7b72ea1858844367960f8c9a
Kai Song <ksong@lbl.gov>
2016-05-01 18:19:10 -0500

    Added java doc for RESTResponseData.java and RESTSessionResponseData.java. all public methods except setters and getters

--- src/main/java/cs428/project/gather/data/response/RESTResponseData.java
+++ src/main/java/cs428/project/gather/data/response/RESTResponseData.java
@@ -4,13 +4,34 @@ import java.util.Date;
 import org.springframework.http.*;
 import org.springframework.validation.*;
 
+/**
+ * 
+ * @author Team Gather
+ * Basic REST interface response data contains the return status, a message, 
+ * and a timestamp. Based on the validator check, this class will provide the 
+ * proper return status.
+ * 
+ */
 public class RESTResponseData {
     protected int status = -1;
     protected String message = "";
     protected long timestamp;
 
+	/**
+	 * Constructor.
+	 * Create an empty response data.  
+	 * 
+	 */
     public RESTResponseData() {}
 
+	/**
+	 * Constructor.
+	 * Create a response data based on return status and message.
+	 * 
+	 * @param status: The return status   
+	 * @param message: The return message
+	 * 
+	 */
     public RESTResponseData(int status, String message) {
         this.status = status;
         Date now = new Date();
@@ -18,14 +39,37 @@ public class RESTResponseData {
         this.message = message;
     }
 
+	/**
+	 * Create a OK response data with a message.
+	 * 
+	 * @param message: The return message
+	 * @return: A OK response data with the given message.
+	 * 
+	 */
     public static ResponseEntity<RESTResponseData> OKResponse(String message) {
         return new ResponseEntity<RESTResponseData>(new RESTResponseData(0, message), HttpStatus.OK);
     }
 
+	/**
+	 * Create a basic response data with return status, a message, and HTTP Status.
+	 * 
+	 * @param status: The return status
+	 * @param message: The return message
+	 * @param httpStatus: The HTTP status
+	 * @return: A response data with return status, a message, and HTTP Status.
+	 * 
+	 */
     public static ResponseEntity<RESTResponseData> response(int status, String message, HttpStatus httpStatus) {
         return new ResponseEntity<RESTResponseData>(new RESTResponseData(status, message), httpStatus);
     }
 
+	/**
+	 * Create a bad request with a proper return status, based on the binding result.
+	 * 
+	 * @param bindingResult: The binding result that contains the error information form the validator  
+	 * @return: A bad request response with a proper return status, based on the binding result.
+	 * 
+	 */
     public static ResponseEntity<RESTResponseData> buildResponse(BindingResult error) {
         String message="";
         int errorCode=-1;
@@ -70,10 +114,6 @@ public class RESTResponseData {
             result = HttpStatus.NOT_FOUND;
             break;
         case -6:
-        	//TODO: If we wish to change this back to UNAUTHORIZED, we need to fix SignInControllerTest.testSignInUserWrongPassword()
-        	//HttpStatus.UNAUTHORIZED causes test to fail since it seems the RestTemplate automatically attempts to retry when this status 
-        	//is received and the retry throws an exception
-//            result = HttpStatus.UNAUTHORIZED;
         	result = HttpStatus.BAD_REQUEST;
             break;
         case -7:
